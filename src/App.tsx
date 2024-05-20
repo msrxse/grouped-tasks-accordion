892d61f04bb0be11d2cc0454aecf92b78ad29fd4
msr <msrxse@gmail.com>
2024-05-20 22:06:44 +0100

    Edit appjs file

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,7 @@ export const App = () => {
   return (
     <div data-testid="app-id" className="py-14 container">
       <div className="relative">
-        <h1 className="text-[40px] satoshi text-custom-black font-medium">Special Rates</h1>
-        <h2 className="text-3xl font-bold underline">Hello world!</h2>
+        <h1 className="text-[40px] satoshi text-custom-black font-medium">Hello World</h1>
       </div>
     </div>
   )
